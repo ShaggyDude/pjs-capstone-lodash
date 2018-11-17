2d09dcfc75d4940b10bb6e1ce9c5afe33a6fb7ca
Scott Romack <scott@romack.net>
2018-11-17 08:59:40 -0600

    Update _.js
    Code cleanup and refactor.

--- lodash/_.js
+++ lodash/_.js
@@ -1,47 +1,36 @@
 
-
 const _ = {
-
-  clamp(number, lower, upper) {
-    number = +number // convert string values to number if need be.
-    lower = +lower
-    upper = +upper
-    numberMax = Math.max(number, lower);
-    numberMin = Math.min(number, upper);
-    return Math.max(Math.min(number, upper), lower);
+  clamp: (number, lower, upper) => {
+    numberMax = Math.max(number, lower)
+    numberMin = Math.min(number, upper)
+    return Math.max(Math.min(number, upper), lower)
   },
   inRange: (number, start, end) => {
+    end === undefined ? end = start : start = 0
     const isInRange = function(number, start, end) {
       return number >= Math.min(start, end) && number < Math.max(start, end)
     }
-    if (end === undefined) {
-      end = start
-      start = 0
-    }
     return isInRange(+number, +start, +end)
   },
-
   words: (string) => {
     return string.split(' ');
   },
   pad: (string, length) => {
-    let difference = length - string.length
-    if (difference <= 0) {
-      difference = 0;
-    }
-    let startPad = Math.floor(difference / 2);
-    let endPad = Math.ceil(difference / 2);
-    paddedString = ' '.repeat(startPad)+string+' '.repeat(endPad)
+    const difference = length - string.length
+    difference <= 0 ? difference = 0
+    const startPad = Math.floor(difference / 2)
+    const endPad = Math.ceil(difference / 2)
+    const padder = ' '
+    paddedString = `${padder.repeat(startPad) string padder.repeat(endPad)}`
     return paddedString
   },
-
   has: (object, key) => {
     return object != null && hasOwnProperty.call(object, key)
   },
   invert: (object) => {
     const invertedObject = {}
     Object.keys(object).forEach((key) => {
-      let value = object[key]
+      const value = object[key]
       if (value != null && typeof value.toString != 'function') {
         value = toString.call(value)
       }
@@ -50,15 +39,10 @@ const _ = {
     return invertedObject
   },
   findKey: (object, predicate) => {
-    let result
-    if (object == null) {
-      return result
-    }
+    const result
+    object == null ? return result
     Object.keys(object).some((key) => {
       const value = object[key]
-
-      console.log('value:' + value)
-
       if (predicate(value, key, object)) {
         result = key
         return true
@@ -66,34 +50,29 @@ const _ = {
     })
     return result
   },
-
   drop: (array, num = 1) => {
     const length = array == null ? 0 : array.length
     return length ? array.slice(num) : []
   },
-  dropWhile(array, predicate) {
-    let newArray = [];
-    for(let x = 0; x < array.length; x++) {
-
-      if(!predicate(array[x], x, array)) {
+  dropWhile: (array, predicate) => {
+    const newArray = [];
+    for (let x = 0; x < array.length; x++) {
+      if (!predicate(array[x], x, array)) {
         newArray = array.slice(x);
       }
     }
     return newArray;
   },
-  chunk(array, size) {
+  chunk: (array, size) => {
     const arrayChunks = [];
-    if(!size) {
-      size = 1;
-    }
-    for(let i = 0; i < array.length - 2; i++) {
+    !size ? size = 1;
+    for (let i = 0; i < array.length - 2; i++) {
       const arrayChunk = array.slice(i + i, i + size);
       arrayChunks.push(arrayChunk);
       size++;
     }
     return arrayChunks;
   }
-
 }
 
 module.exports = _;
